b948f627925f29c36875eab852e642aee40dba5b
KDJ <ehdwn7531@naver.com>
2022-09-13 01:43:01 +0900

    Part 3 : 이동 불가 지역 설정하기(RayCast)
    - RaycastHit2D와 Physics2D.LineCast를 사용하여 이동 불가 처리

--- RPG MAKER GAME/Assets/Scripts/MovingObject.cs
+++ RPG MAKER GAME/Assets/Scripts/MovingObject.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+    private BoxCollider2D boxCollider;
+    public LayerMask layerMask;
+
     public float speed;
 
     private Vector3 vector;
@@ -23,6 +26,7 @@ public class MovingObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
     }
 
@@ -49,17 +53,29 @@ public class MovingObject : MonoBehaviour
             animator.SetFloat("DirX", vector.x);
             animator.SetFloat("DirY", vector.y);
 
+            RaycastHit2D hit;
+
+            Vector2 start = transform.position; //캐릭터의 현재 위치 값 
+            Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount); // 캐릭터가 이동하고자 하는 위치 값
+
+            boxCollider.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            boxCollider.enabled = true;
+
+            if (hit.transform != null)
+                break;
+
             animator.SetBool("Walking", true);
 
             while (currentWalkCount < walkCount)
             {
                 if (vector.x != 0)
                 {
-                    transform.Translate(vector.x * (applyRunSpeed + speed) * Time.deltaTime, 0, 0);
+                    transform.Translate(vector.x * (applyRunSpeed + speed) , 0, 0);
                 }
                 else if (vector.y != 0)
                 {
-                    transform.Translate(0, vector.y * (applyRunSpeed + speed) * Time.deltaTime, 0);
+                    transform.Translate(0, vector.y * (applyRunSpeed + speed) , 0);
                 }
 
                 if (applyRunFlag)
