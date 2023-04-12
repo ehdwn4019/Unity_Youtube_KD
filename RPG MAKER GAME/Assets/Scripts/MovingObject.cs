027e1580b22108ffd6e63740769b2ba362ae4cee
KDJ <ehdwn7531@naver.com>
2023-04-13 02:31:23 +0900

    Part 5 : 맵 제작하기
    - 유니티 맵툴 사용하여 맵 제작 ( 기능 좀더 확인해보기 )

--- RPG MAKER GAME/Assets/Scripts/MovingObject.cs
+++ RPG MAKER GAME/Assets/Scripts/MovingObject.cs
@@ -106,4 +106,9 @@ public class MovingObject : MonoBehaviour
             }
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
 }
